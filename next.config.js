9932bbfe9ea6d721ba02ffe6d865142d4b6e5a92
Verdict <faslah9@gmail.com>
2024-10-28 03:55:28 +0300

    -

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
